cff9759f3f3639285930ffd04b86cba9f95c0a14
Matthew Holland <mtholland10@gmail.com>
2025-04-01 21:49:08 -0500

    Remove dashboard html Update project name

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,49 +1,7 @@
-import { useAppContext } from 'hooks';
-
-import { Link } from 'components/Link';
-
-import { useGetWeather } from 'networking/queryClient/hooks';
-
 export default function DashboardPage() {
-  const appState = useAppContext();
-  const { weatherData } = useGetWeather();
-
   return (
     <div className="bg-slate-900 h-screen">
-      <div className="absolute left-4 top-4">
-        <Link url="/about" text="About >" />
-      </div>
-
-      {weatherData ? (
-        <div className="absolute right-4 top-4">
-          <p>Austin Temp: {weatherData.current.temp_f} F</p>
-        </div>
-      ) : null}
-
-      <div className="flex flex-col items-center mb-30">
-        <h1 className="pt-16 md:pt-10 text-center">Fluid Interaction Layer</h1>
-        <h3 className="text-amber-600">Hello, {appState.user}</h3>
-      </div>
-
-      <div className="flex flex-col items-center justify-center px-6 w-screen gap-10 md:gap-20 bg-slate-900">
-        <div className="flex flex-col md:flex-row w-screen px-6 gap-10 h-96">
-          <div className="flex flex-1 md:flex-1/6 items-center justify-center font-bold bg-amber-500 rounded-sm">
-            <p className="px-4 text-center">Frequency Modulation</p>
-          </div>
-
-          <div className="flex flex-1 md:flex-4/6 items-center justify-center font-bold bg-blue-400 rounded-sm">
-            <p className="px-4 text-center">Intelligence Waveform Panel</p>
-          </div>
-
-          <div className="flex flex-1 md:flex-1/6 items-center justify-center font-bold bg-purple-400 rounded-sm">
-            <p className="px-4 text-center">Thought Interaction</p>
-          </div>
-        </div>
-
-        <div className="flex w-full items-center justify-center font-bold h-24 md:h-48 md:w-9/12 bg-pink-400 rounded-sm">
-          <p className="px-4 text-center">Audio Resonance</p>
-        </div>
-      </div>
+      <p>test</p>
     </div>
   );
 }
